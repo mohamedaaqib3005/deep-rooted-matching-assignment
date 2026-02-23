9970922bad3fd27f029217c21b3747f1a2e4d896
Aaqib <drop.mohamedaaqib@gmail.com>
2026-02-23 13:39:09 +0530

    write algorithm

---
+++ matchmaker.js
@@ -0,0 +1,6 @@
+// PSUEDOCODE
+
+//---> Check for each demand if there is a supply available.
+//--->If no supply is available at that moment move to the next demand follow FIFO
+//--->Only make the deal if the demand price is more or equal to supply price and demand required quantity is less than or equal to supply quantity
+//--->If deal is done then display in the format of demand order id followed by supply order id, price/kg & finally quantity for which deal is done.
