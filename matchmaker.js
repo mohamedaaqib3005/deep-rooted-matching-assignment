70a594fff4ec6c518f5f49615b604a26d9fe197b
Aaqib <drop.mohamedaaqib@gmail.com>
2026-02-24 07:38:46 +0530

    write psuedocode for main function

--- matchmaker.js
+++ matchmaker.js
@@ -4,3 +4,17 @@
 //--->If no supply is available at that moment move to the next demand follow FIFO
 //--->Only make the deal if the demand price is more or equal to supply price and demand required quantity is less than or equal to supply quantity
 //--->If deal is done then display in the format of demand order id followed by supply order id, price/kg & finally quantity for which deal is done.
+
+
+/**
+ * Returns the matched outputData
+ * @param {string} inputData
+ * @returns {string}
+ */
+function matchMaker(inputData) {
+  // Parse the input  separate both demand and supply  into different objects;
+  // Match for each supply what is the most profitable demand requirement ;
+  // Format the match and return the output in the desired format;
+}
+
+
