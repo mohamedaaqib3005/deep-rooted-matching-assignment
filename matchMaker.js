30ad72f44341ef375534f9470e6d626dca8963bf
Aaqib <drop.mohamedaaqib@gmail.com>
2026-02-25 20:39:40 +0530

    write basic matching logic

--- matchMaker.js
+++ matchMaker.js
@@ -11,14 +11,9 @@
  * @param {string} inputData
  * @returns {string}
  */
-
-
-
-
-
 function matchMaker(input) {
   const inputData = `
-   d1 09:47 tomato 110/kg 1kg
+d1 09:47 tomato 110/kg 1kg
 d2 09:45 potato 110/kg 10kg
 d3 09:48 tomato 110/kg 10kg
 
@@ -41,42 +36,65 @@ s4 09:45 tomato 110/kg 11kg
 
         }
       })
+
   }
 
 
 
-  function formatSupply(parsedData) {
+  function formatSupply(parsed) {
     // format the parsed supplyData into an object
+    let supply = [];
+    for (let object of parsed) {
+      console.log(object)
+      if (object.id.startsWith("s"))
+        supply.push(object);
+    }
+    return supply;
   }
 
-  function formatDemand(parsedData) {
+  function formatDemand(parsed) {
     // format the parsed demandData into an object
-
+    let demand = [];
+    for (let object of parsed) {
+      if (object.id.startsWith("d")) {
+        demand.push(object);
+      }
+    }
+    return demand;
   }
 
-  function matcher(formatedSupply, formatedDemand) {
+  function matcher(supply, demand) {
     // Match for each supply what is the most profitable demand requirement ;
+    let matchedArray = []
+    for (let supplyEntry of supply) {
+      for (let demandEntry of demand) {
+        if (supplyEntry.vegetable === demandEntry.vegetable & supplyEntry.quantity >= demandEntry.quantity & supplyEntry.price <= demandEntry.price) {
+          matchedArray.push(
+            {
+              matched: {
+                demand: demandEntry,
+                supply: supplyEntry
+              },
+            }
+          );
+        }
+      }
+    }
+    return matchedArray;
   }
 
   // Format the match and return the output in the desired format;
   function formatData(matchedData) {
     // Format the matched demand and supply datatype into output of string format and return it ;
   }
-  console.log(parseInput(inputData));
+  const parsed = parseInput(inputData)
+  console.log(parseInput(inputData))
+  console.log(formatSupply(parsed))
+  console.log(formatDemand(parsed))
+  console.dir(matcher(formatSupply(parsed), formatDemand(parsed)), { depth: null })
   // return OutputData;
 }
 matchMaker()
 
-// matchMaker(inputData)
-
-
-
-// function parseInput(inputData) {
-//   const parsedInput = inputData.split();
-//   console.log(parsedInput)
-// }
-
-
-// parseInput(inputData)
 
 
